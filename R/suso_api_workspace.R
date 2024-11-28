8f276c74f1278a3e07393f9ac5459509b83778b3
tfho <51022350+tfho@users.noreply.github.com>
2024-11-28 10:25:21 +0000

    Update suso_api_workspace.R

--- R/suso_api_workspace.R
+++ R/suso_api_workspace.R
@@ -56,8 +56,8 @@ suso_getWorkspace <- function(server = suso_get_api_key("susoServer"),
     ## 1.2 call
     test_detail <- GET(url = build_url(url),
                        auth,
-                       httr::config(ssl_verifypeer = FALSE),
-                       httr::write_disk(aJsonFile, overwrite = T))
+                       httr::write_disk(aJsonFile, overwrite = T),
+                       config = httr::config(ssl_verifypeer = FALSE, ssl_verifyhost = FALSE))
     check_response(test_detail)
     test_json <- jsonlite::fromJSON(aJsonFile)
     test_json<-data.table(test_json$Workspaces)
